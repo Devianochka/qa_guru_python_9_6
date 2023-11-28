ca213576173c9854f0f83f0abe65cd0473836009
Diana Sagaeva <d_sagaeva@mail.ru>
2023-11-28 20:53:09 +0300

    на проверку

--- test_homework.py
+++ test_homework.py
@@ -19,19 +19,28 @@ def test_dark_theme_by_time():
     """
 
 
-current_time = time(hour=16)
-dark_theme_enabled_by_user = True
+def test_dark_theme_by_time_and_user_choice():
 
-if dark_theme_enabled_by_user:
-    is_dark_theme = True
-elif dark_theme_enabled_by_user is None:
-    if current_time >= time(hour=22) or current_time <= time(hour=6):
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+
+    if dark_theme_enabled_by_user:
         is_dark_theme = True
+    elif dark_theme_enabled_by_user is None:
+        if current_time >= time(hour=22) or current_time <= time(hour=6):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
     else:
         is_dark_theme = False
-else:
-    is_dark_theme = False
-assert is_dark_theme is True
+    assert is_dark_theme is True
 
 
 def test_find_suitable_user():
